3db356f1aed1cf4312ddd1a769788a53831ac359
Brian Smith <smith.brianr@gmail.com>
2013-11-09 10:34:09 -0600

    Updated Links

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rxngif::Application.routes.draw do
+ # CREATE
+  get("/new_picture_form", { :controller => "pictures", :action => "new", :as => "new_picture" })
+  get("/create_picture", { :controller => "pictures", :action => "create" })
 
   # READ
   get("/picture_details/:id", { :controller => "pictures", :action => "show" })
   get("/all_pictures", { :controller => "pictures", :action => "index" })
 
-  # CREATE
-  get("/new_picture_form", { :controller => "pictures", :action => "new", :as => "new_picture" })
-  get("/create_picture", { :controller => "pictures", :action => "create" })
+ # UPDATE
+  get("/edit_picture_form/:id", { :controller => "pictures", :action => "edit" })
+  get("/update_picture/:id", { :controller => "pictures", :action => "update" })
 
  # DELETE
   delete("/delete_picture/:id", { :controller => "pictures", :action => "destroy" })
 
- # UPDATE
-  get("/edit_picture_form/:id", { :controller => "pictures", :action => "edit" })
-  get("/update_picture/:id", { :controller => "pictures", :action => "update" })
 end
